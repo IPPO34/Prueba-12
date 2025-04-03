b41f5abd86a6ab61ca5bd36bfb1af401548d8126
IPPO34 <carlostristan154@gmail.com>
2025-04-03 15:24:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("clickToReveal").addEventListener("click", function() {
+    // Cambiar la visibilidad del mensaje
+    document.getElementById("welcome").style.display = "none";
+    document.getElementById("message").style.display = "block";
+});
